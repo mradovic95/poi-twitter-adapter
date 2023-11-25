2690eaeabc598c41732a387faa56f9e575ea4b98
mihailoradovic <mihailo.radovic@gmail.com>
2023-11-25 13:47:32 +0200

    change field to string

--- src/controller/TwitterController.js
+++ src/controller/TwitterController.js
@@ -9,7 +9,7 @@ router.get('/api/v1/tweet/:id/tweet-reach', async (req, res, next) => {
 
 		res.status(200).send({
 			data: {
-				tweetReach: 1 + Math.floor(Math.random() * 1000000000)
+				tweetReach: (1 + Math.floor(Math.random() * 1000000000)).toString()
 			}
 		});
 	} catch (error) {
