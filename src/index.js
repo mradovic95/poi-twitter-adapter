b115ea04cca0bd6d887e8f0fe817dd38087cad0b
mihailoradovic <mihailo.radovic@gmail.com>
2023-11-25 23:16:43 +0200

    change field to string

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const express = require("express");
 const app = express();
 
 const twitterController = require("./controller/TwitterController");
+const influencerContractController = require("./controller/InfluencerContractController");
 app.use(twitterController);
+app.use(influencerContractController);
 
 app.listen(8080, () => {
 	console.log("Application is running on port: 8080")
