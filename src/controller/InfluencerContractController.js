353c09fc056c6f4cf1afb57233ef251ebf11f450
mihailoradovic <mihailo.radovic@gmail.com>
2023-11-26 01:58:09 +0200

    change field to string

--- src/controller/InfluencerContractController.js
+++ src/controller/InfluencerContractController.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const Web3 = require("web3");
 const web3 = new Web3("https://sepolia.infura.io/v3/d7b60fd99b624406ac0943d8f301c4d9");
 const jsonInterface = require("../POI.json").abi;
-let contract = new web3.eth.Contract(jsonInterface, "0xbaAC51DEA992D7f0eDb4Ef971016c8c70d57BA7d");
+let contract = new web3.eth.Contract(jsonInterface, "0x9C648d8B460E89F29ad71ee3C9204d6c502c7Cd1");
 let privateKey = "b547298ba25e6394beec9ded0f8e431c0fab50948dea80844ece8bf468a74650";
 
 const account = web3.eth.accounts.privateKeyToAccount(privateKey);
