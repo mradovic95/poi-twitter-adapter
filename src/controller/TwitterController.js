782667dc700dabb2e3d397b130da782e96b4376a
mihailoradovic <mihailo.radovic@gmail.com>
2023-11-25 17:20:41 +0200

    change field to string

--- src/controller/TwitterController.js
+++ src/controller/TwitterController.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/api/v1/tweet', async (req, res, next) => {
 
 	try {
-
+		console.log("requestedReach:" + req.query["reach"] + "," + "requestedLikes:" + req.query["likes"])
 		res.status(200).send({
 			data: {
 				tweetReachAndLikes: req.query["reach"] + "," + req.query["likes"],
