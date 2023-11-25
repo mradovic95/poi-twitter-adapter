71f672f330d914485198e18b76504972b84c5f18
mihailoradovic <mihailo.radovic@gmail.com>
2023-11-25 20:46:30 +0200

    change field to string

--- src/controller/TwitterController.js
+++ src/controller/TwitterController.js
@@ -18,4 +18,18 @@ router.get('/api/v1/tweet', async (req, res, next) => {
 	}
 });
 
+router.get('/api/v1/tweet/content', async (req, res, next) => {
+
+	try {
+		res.status(200).send({
+			data: {
+				content: "First Stripe POS terminal I've seen in the wild. Very cool (and makes a lot of sense!)"
+			}
+		});
+	} catch (error) {
+
+		res.status(500).send("Internal Server Error");
+	}
+});
+
 module.exports = router;
